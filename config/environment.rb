322ff2a93bf11f7a780374070cfd6fb1e0d78bcd
Micah Alcorn <mail@micahalcorn.com>
2011-05-08 15:19:11 -0500

    Wrong source.

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Teresa::Application.initialize!
-
-Picasa.config do |c|
-	c.google_user = 'teresaalcorn'
-end
